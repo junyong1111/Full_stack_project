92b23c25b6125c1ff23a5f50cfb6cd29550e9b0b
Junyong <jypark93@kookmin.ac.kr>
2023-08-19 00:06:02 +0900

    feat_db : 기본 post, get 실습 추가 추후 변경해야함

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -3,5 +3,5 @@ from .views import hello_world
 
 app_name = "accountapp"
 urlpatterns = [
-    path('hello_world/', hello_world)
+    path('hello_world/', hello_world, name = "hello_world")
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,20 @@
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
 
 # Create your views here.
 def hello_world(request):
-    return render(request, "accountapp/hello_world.html")
+    if request.method == "POST":
+        temp = request.POST.get("account_input")
+        newModel = HelloWorld()
+        newModel.text = temp
+        newModel.save()
+        HelloWorldList = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse("accountapp:hello_world"))
+    else:
+        HelloWorldList = HelloWorld.objects.all()
+        return render(request, "accountapp/hello_world.html", context={"HelloWorldList" : HelloWorldList})
